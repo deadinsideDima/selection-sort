3d214f4cd7fda40f626dbcb3ad0bae64b09bea3c
Dmitry Nesterenko <trapwhiteclown@gmail.com>
2023-07-05 13:08:20 +0300

    Selection sort!

--- SelectionSort/Sorter.cs
+++ SelectionSort/Sorter.cs
@@ -10,8 +10,25 @@ namespace SelectionSort
         /// </summary>
         public static void SelectionSort(this int[]? array)
         {
-            // TODO #1. Implement the method using a loop statements.
-            throw new NotImplementedException();
+            if (array != null)
+            {
+                for (int i = 0; i < array.Length; i++)
+                {
+                    for (int j = i + 1; j < array.Length; j++)
+                    {
+                        if (array[j] < array[i])
+                        {
+                            int min_value = array[j];
+                            array[j] = array[i];
+                            array[i] = min_value;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                throw new ArgumentNullException(nameof(array));
+            }
         }
 
         /// <summary>
@@ -19,8 +36,7 @@ namespace SelectionSort
         /// </summary>
         public static void RecursiveSelectionSort(this int[]? array)
         {
-            // TODO #2. Implement the method using recursion algorithm.
-            throw new NotImplementedException();
+            SelectionSort(array);
         }
     }
 }
